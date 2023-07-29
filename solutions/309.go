9e7dee4679300fba4b8f0b3480d8b96c33afb44c
Alvin <hello@alvin.codes>
2023-07-29 19:22:41 +0100

    👯‍♀️ 309

--- solutions/309.go
+++ solutions/309.go
@@ -1,16 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
+// This week's question:
+// Given a string, separate it into groups of non-space equal characters, sorted.
+//
+// Example:
+// explodeString('Ahh, abracadabra!')
+// ['!',',','A','aaaaa','bb','c','d','hh','rr']
 func explode(str string) []string{
-	fmt.Println(str)
+	var letters []string = strings.Split(str, "");
+	var result []string;
+	var acc = make(map[string]int)
 
-	var result = []string{"Hello"}
+	for _,letter := range letters{
+		acc[letter]++;
+	}
+
+	for k, v := range acc{
+		 str := "";
+		for i := 0; i < v; i++ {
+			str = str + k
+		}
+		result = append(result, str)
+	}
+
+	sort.Strings(result)
 	return result
 }
 
 
 func main(){
-	explode("hello");
-
+	 res := explode("Ahh, abracadabra!")
+	fmt.Println(res)
 }
